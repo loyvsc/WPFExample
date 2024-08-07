e3b7085c756c2e316b978fa7cdd230b18c80727e
Valery Varaksa <varaksav62@gmail.com>
2024-08-07 22:07:33 +0300

    Implement host builder

--- WPFExample.Presentation/App.xaml.cs
+++ WPFExample.Presentation/App.xaml.cs
@@ -1,12 +1,45 @@
-﻿using System.Configuration;
-using System.Data;
-using System.Windows;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WPFExample.Presentation.Extensions;
+using WPFExample.Presentation.Views;
 
 namespace WPFExample.Presentation;
 
-/// <summary>
-/// Interaction logic for App.xaml
-/// </summary>
 public partial class App : Application
 {
+    private readonly IHost _host;
+    
+    public App()
+    {
+        _host = CreateHostBuilder().Build();
+        _host.Start();
+    }
+
+    private IHostBuilder CreateHostBuilder()
+    {
+        return new HostBuilder()
+            .AddHttpUtilities()
+            .AddServices()
+            .AddViewModels()
+            .AddViews();
+    }
+    
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        _host.Start();
+
+        var window = _host.Services.GetRequiredService<MainWindow>();
+        window.Show();
+
+        base.OnStartup(e);
+    }
+
+    protected override async void OnExit(ExitEventArgs e)
+    {
+        await _host.StopAsync();
+        _host.Dispose();
+
+        base.OnExit(e);
+    }
 }
