0b08eafd59666d1a529cf1369f73708bbc68939f
Valery Varaksa <varaksav62@gmail.com>
2024-08-07 20:35:00 +0300

    Create MainWindowViewModel

---
+++ WPFExample.Presentation/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.Logging;
+
+namespace WPFExample.Presentation.ViewModels;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    public MainWindowViewModel(ILogger<MainWindowViewModel> logger) : base(logger)
+    {
+        
+    }
+}
