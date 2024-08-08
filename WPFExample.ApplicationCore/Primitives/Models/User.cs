e9e2e4d14db102936b63fc68c870786bb96148a1
Valery Varaksa <varaksav62@gmail.com>
2024-08-08 12:59:45 +0300

    Update User

--- WPFExample.ApplicationCore/Primitives/Models/User.cs
+++ WPFExample.ApplicationCore/Primitives/Models/User.cs
@@ -5,14 +5,30 @@ namespace WPFExample.ApplicationCore.Primitives.Models;
 public class User
 {
     [JsonPropertyName("id")] public int Id { get; set; }
-    [JsonPropertyName("username")] public string Username { get; set; } = string.Empty;
-    [JsonPropertyName("firstName")] public string Firstname { get; set; } = string.Empty;
-    [JsonPropertyName("lastName")] public string Lastname { get; set; } = string.Empty;
-    [JsonPropertyName("email")] public string Email { get; set; } = string.Empty;
-    [JsonPropertyName("password")] public string Password { get; set; } = string.Empty;
-    [JsonPropertyName("phone")] public int Phone { get; set; }
+    [JsonPropertyName("username")] public string Username { get; set; }
+    [JsonPropertyName("firstName")] public string Firstname { get; set; }
+    [JsonPropertyName("lastName")] public string Lastname { get; set; }
+    [JsonPropertyName("email")] public string Email { get; set; }
+    [JsonPropertyName("password")] public string Password { get; set; }
+    [JsonPropertyName("phone")] public string Phone { get; set; }
     [JsonPropertyName("userStatus")] public int UserStatus { get; set; }
     
     [JsonIgnore] public string Token { get; set; } = string.Empty;
     [JsonIgnore] public DateTime TokenExpiresIn { get; set; } = DateTime.MinValue;
+
+    public User()
+    {
+    }
+
+    public User(int id, string username, string firstname, string lastname, string email, string password, string phone, int userStatus)
+    {
+        Id = id;
+        Username = username;
+        Firstname = firstname;
+        Lastname = lastname;
+        Email = email;
+        Password = password;
+        Phone = phone;
+        UserStatus = userStatus;
+    }
 }
