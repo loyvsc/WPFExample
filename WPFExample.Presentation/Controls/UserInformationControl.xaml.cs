7588cb89330e804567615654b6a3a9f53e6a49bf
Valery Varaksa <varaksav62@gmail.com>
2024-08-08 13:02:02 +0300

    Update UserInformationControl

---
+++ WPFExample.Presentation/Controls/UserInformationControl.xaml.cs
@@ -0,0 +1,11 @@
+using System.Windows.Controls;
+
+namespace WPFExample.Presentation.Controls;
+
+public partial class UserInformationControl : UserControl
+{
+    public UserInformationControl()
+    {
+        InitializeComponent();
+    }
+}
